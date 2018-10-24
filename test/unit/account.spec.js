b44e97e60ff9478f8ffd5545851f6d6fbf12b247
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-10-24 11:55:20 -0400

    Use faker to generate fake data

--- test/unit/account.spec.js
+++ test/unit/account.spec.js
@@ -4,6 +4,8 @@ const { ServiceBroker } = require("moleculer");
 const { ValidationError } = require("moleculer").Errors;
 const TestService = require("../../services/account.service");
 
+const faker = require("faker");
+
 describe("Test 'account' service", () => {
   let broker = new ServiceBroker();
   broker.createService(TestService);
@@ -13,18 +15,20 @@ describe("Test 'account' service", () => {
 
   describe("Test 'account.setWallet' action", () => {
     it("should return the wallet which is passed", async () => {
+      const userId = faker.random.word();
+      const address = faker.random.uuid();
       return expect(
-        broker.call("account.setWallet", { userId: "1", address: "abc" })
-      ).resolves.toEqual({ userId: "1", address: "abc" });
+        broker.call("account.setWallet", { userId, address })
+      ).resolves.toEqual({ userId, address });
     });
 
     it("should set the wallet which is returned with get", async () => {
-      await broker.call("account.setWallet", { userId: "1", address: "abc" });
+      const userId = faker.random.word();
+      const address = faker.random.uuid();
+      await broker.call("account.setWallet", { userId, address });
       return expect(
-        broker.call("account.getWallet", { userId: "1" })
-      ).resolves.toEqual({
-        address: "abc"
-      });
+        broker.call("account.getWallet", { userId })
+      ).resolves.toEqual({ address });
     });
 
     it("should reject an ValidationError", () => {
